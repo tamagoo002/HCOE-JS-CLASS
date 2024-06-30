52245061fdefe3aab3ecabce5c1a41ef72866397
Tamago <anamikarai0020806@gmail.com>
2024-06-30 13:55:03 +0545

    updated day 3

--- index.js
+++ index.js
@@ -506,8 +506,8 @@
 //     return largest;
 // };
 // console.log(largestcountry(countries));
-const sysrole = ["admin", "manager"];
-const userrole = ["user", "receptionist", "manager"];
+// const sysrole = ["admin", "manager"];
+// const userrole = ["user", "receptionist", "manager"];
 
 // Compare two roles and return boolean if role matches
 //const getcommon = (food1, food2) => {
@@ -525,3 +525,41 @@ const userrole = ["user", "receptionist", "manager"];
 //   sysrole.some((role) => userrole.includes(role));
 
 // console.log(rolematch(sysrole, userrole));
+
+///
+const characters = [
+  {
+    name: "Luke Skywalker",
+    height: "172",
+    mass: "77",
+    eye_color: "blue",
+    gender: "male",
+  },
+  {
+    name: "Darth Vader",
+    height: "202",
+    mass: "136",
+    eye_color: "yellow",
+    gender: "male",
+  },
+  {
+    name: "Leia Organa",
+    height: "150",
+    mass: "49",
+    eye_color: "brown",
+    gender: "female",
+  },
+  {
+    name: "Anakin Skywalker",
+    height: "188",
+    mass: "84",
+    eye_color: "blue",
+    gender: "male",
+  },
+];
+const charactersByEyeColor = characters.reduce((acc, { eye_color }) => {
+  acc[eye_color] = (acc[eye_color] || 0) + 1;
+  return acc;
+}, {});
+
+console.log("Total number of characters by eye color:", charactersByEyeColor);
